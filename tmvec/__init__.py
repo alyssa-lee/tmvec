dc9fd80c94129df484508f968177844ad5491ec1
valentynbez <walentyn.bzsz@gmail.com>
2024-07-11 11:03:45 +0200

    fix: correct links

--- tmvec/__init__.py
+++ tmvec/__init__.py
@@ -3,6 +3,6 @@ __author__ = "Valentyn Bezshapkin <valentyn.bezshapkin@micro.biol.ethz.ch>"
 __licencse__ = "BSD-3-Clause"
 
 TMVEC_SEQ_LIM = 1024
-TMVEC_REPO = 'valentynbez/tmvec'
+TMVEC_REPO = 'scikit-bio/tmvec'
 PLM_REPO = "Rostlab/prot_t5_xl_uniref50"
-PLM_ONNX = "valentynbez/prot-t5-xl-uniref50-onnx"
+PLM_ONNX = "scikit-bio/prot-t5-xl-uniref50-onnx"
